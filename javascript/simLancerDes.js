89dc6be30c0a3723fad9b2c28db6f54be58eecf1
RoxaneKirchmeyer <roxane.kirchmeyer@gmail.com>
2024-01-10 12:01:37 +0100

    Add patrol images

--- javascript/simLancerDes.js
+++ javascript/simLancerDes.js
@@ -5,7 +5,7 @@ function lancerDe() {
   let nombreFaces = parseInt(document.querySelector('input[name="deFaces"]:checked').value)
   let nombreRandom = Math.floor(Math.random() * nombreFaces) + 1;
   console.log("le dé :", nombreFaces, "a donné le résultat " + nombreRandom)
-  imageDes.src = "img/Die" + nombreRandom + ".png"
+  imageDes.src = "img/dices/Die" + nombreRandom + ".png"
 }
 
 const clickButton = document.getElementById("lancerDe");
